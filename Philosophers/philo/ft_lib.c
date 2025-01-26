5df07c5c5bd72a6ecfc034675e7db2628a021a91
justJuste <jules.longin1@gmail.com>
2025-01-27 00:42:16 +0100

    catch philo

--- Philosophers/philo/ft_lib.c
+++ Philosophers/philo/ft_lib.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_philosophers.h                                  :+:      :+:    :+:   */
+/*   ft_lib.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By:                                            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -9,58 +9,27 @@
 /*   Updated:   by Just'                              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#ifndef FT_PHILOSOPHERS_H
-# define FT_PHILOSOPHERS_H
+#include "ft_philosophers.h"
 
-// Standard Libraries
-
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
-
-// thread
-# include <pthread.h>
-
-// gettimeofday
-#include <sys/time.h>
-
-// Structures
-
-typedef struct pthread_mutex_t	t_mtx;
-typedef struct s_table		t_table;
-typedef struct s_philo		t_philo;
-typedef struct s_fork		t_fork;
-
-typedef struct s_table
-{
-	int			nb_philo;
-	int			time_to_die;
-	int			time_to_eat;
-	int			time_to_sleep;
-	int			meals_taken;
-	t_philo		*philo;
-}	t_table;
-
-typedef struct s_philo
-{
-	int			id;
-	pthread_t	id_thread;
-}	t_philo;
-
-typedef struct s_fork
-{
-}	t_fork;
-
-//	---------- MY CODE ----------
-
-// ft_philosophers.c
-
-// ft_utils.c
-
-void		ft_error(t_table *table, int error);
-
-// ft_lib.c
-
+// -----------------------PROTOTYPE-------------------------
 void		*ft_calloc(size_t count, size_t size);
+// ---------------------------------------------------------
 
-# endif
+void	*ft_calloc(size_t count, size_t size)
+{
+	size_t		i;
+	size_t		tot;
+	void		*ptr;
+
+	i = 0;
+	tot = count * size;
+	ptr = malloc(tot);
+	if (!ptr)
+		return (NULL);
+	while (i < tot)
+	{
+		((char *)ptr)[i] = 0;
+		i++;
+	}
+	return (ptr);
+}
--- Philosophers/philo/ft_philosophers.c
+++ Philosophers/philo/ft_philosophers.c
@@ -12,12 +12,67 @@
 #include "ft_philosophers.h"
 
 // -----------------------PROTOTYPE-------------------------
+int			main(int argc, const char **argv);
+t_table		*ft_init(void);
+void		ft_create_philosophers(t_table *table);
+void		ft_end_simulation(t_table *table);
+void		*ft_routine(void *data);
 // ---------------------------------------------------------
 
 static pthread_mutex_t	c;
 static int		a = 0;
 
-void	*ft_routine(void *b)
+//if (argc != 5 || argc != 6)
+//	return (ft_error(table, 255), 255);
+int	main(int argc, const char **argv)
+{
+	t_table		*table;
+
+	table = ft_init();
+	pthread_mutex_init(&c, NULL);
+	ft_create_philosophers(table);
+	ft_end_simulation(table);
+	pthread_mutex_destroy(&c);
+	free(table);
+	return (0);
+}
+
+void	ft_end_simulation(t_table *table)
+{
+	int		i;
+
+	i = 0;
+	while (i < table->nb_philo)
+	{
+		pthread_join(table->philo[i].id_thread, NULL);
+		i++;
+	}
+}
+
+void	ft_create_philosophers(t_table *table)
+{
+	int			i;
+
+	i = 0;
+	table->nb_philo = 5;
+	table->philo = ft_calloc(table->nb_philo, sizeof(t_philo));
+	while (i < table->nb_philo)
+	{
+		pthread_create(&table->philo[i].id_thread,
+			NULL, ft_routine, &table->philo[i]);
+		i++;
+	}
+}
+
+t_table	*ft_init(void)
+{
+	t_table		*table;
+
+	table = ft_calloc(1, sizeof(t_table));
+	return (table);
+}
+
+void	*ft_routine(void *data)
 {
 	pthread_mutex_lock(&c);
 	printf("%d", a++);
@@ -25,22 +80,3 @@ void	*ft_routine(void *b)
 	return (NULL);
 }
 
-int	main(int argc, const char *argv[])
-{
-	pthread_t	thread[3];
-	t_table		*table;
-
-	table = NULL;
-	//if (argc != 5 || argc != 6)
-	//	return (ft_error(table, 255), 255);
-	pthread_mutex_init(&c, NULL);
-	pthread_create(&thread[0], NULL, ft_routine, NULL);
-	pthread_create(&thread[1], NULL, ft_routine, NULL);
-	pthread_create(&thread[2], NULL, ft_routine, NULL);
-	pthread_join(thread[0], NULL);
-	pthread_join(thread[1], NULL);
-	pthread_join(thread[2], NULL);
-	//pthread_mutex_destroy(&c);
-	printf("\n");
-	return (0);
-}
