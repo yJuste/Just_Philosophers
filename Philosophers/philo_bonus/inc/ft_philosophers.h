6fcbc2e5c6890e33efa360215c651ed2dedab229
justJuste <jules.longin1@gmail.com>
2025-02-05 13:28:25 +0100

    add base for bonus part

---
+++ Philosophers/philo_bonus/inc/ft_philosophers.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_philosophers.h                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By:                                            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created:   by Just'                               #+#    #+#             */
+/*   Updated:   by Just'                              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef FT_PHILOSOPHERS_H
+# define FT_PHILOSOPHERS_H
+
+// Standard Librairies
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+// thread
+# include <pthread.h>
+
+// gettimeofday
+# include <sys/time.h>
+
+//	---------- MY CODE ----------
+
+// main.c
+
+int			main(void);
+
+#endif
